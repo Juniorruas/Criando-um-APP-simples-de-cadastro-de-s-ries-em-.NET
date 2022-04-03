63b531af3e967f78e61b14957cc49cbe8e951873
Juniorruas <adilson.ruas14@gmail.com>
2022-04-02 23:03:44 -0300

    programação final

--- classes/Serie.cs
+++ classes/Serie.cs
@@ -1,35 +1,15 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
     {
-        //Atributos
-        public Serie(Genero genero, string titulo, int ano)
-        {
-            this.Genero = genero;
-            this.Titulo = titulo;
-            this.Ano = ano;
-
-        }
-        public Serie(Genero genero, string titulo, string descricao, int ano)
-        {
-            this.Genero = genero;
-            this.Titulo = titulo;
-            this.Descricao = descricao;
-            this.Ano = ano;
-
-        }
-        public Serie(Genero genero, string titulo, string descricao, int ano)
-        {
-            this.Genero = genero;
-            this.Titulo = titulo;
-            this.Descricao = descricao;
-            this.Ano = ano;
-
-        }
+        //Atributos        
         private Genero Genero { get; set; }
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         //Métodos
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -39,6 +19,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -48,6 +29,7 @@ namespace DIO.Series
             retorno += "Título: " + this.Titulo + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Inicío: " + this.Ano;
+            retorno += "Excluído: " + this.Excluido;
             return retorno;
         }
 
@@ -56,9 +38,16 @@ namespace DIO.Series
             return this.Titulo;
         }
 
-        internal int retornaId()
+        public int retornaId()
         {
             return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir() {
+            this.Excluido = true;
         }
     }
 }
